2ec6cdf119294f27b1fb8c2788960caa2c5e777f
DEV8020 <datardevdatt8020@gmail.com>
2023-04-26 18:39:56 +0530

    profiledata controller

---
+++ HomeMedicare/app/Controller/ProfileDataController.js
@@ -0,0 +1,40 @@
+import GlobalServiceHandler from "./GlobalServiceController";
+import APIURLUtilities from "./APIUrlUtilities";
+
+const GetUserProfileDetails = async (props) => {
+  //   console.log(props.authToken);
+  var UserName;
+
+  try {
+    const username = await APIURLUtilities.getLoggedInUserName();
+    console.log(username); // "arshdeepworker"
+    UserName = username;
+  } catch (error) {
+    console.log(error);
+  }
+  await GlobalServiceHandler.hitDataGetService({
+    childURL:
+      APIURLUtilities.getFieldWorkerProfileDataAPIChildURLKeys() + +UserName,
+
+    responseDataHandler: (profileServiceData) => {
+      //Login respone parsing in case of Success...
+      if (profileServiceData.responseError === null) {
+        props.userProfileResponseHandler({
+          isProfileDataFlag: true,
+          profileData: profileServiceData.responseData.data,
+          errorMessage: null,
+        });
+      }
+      //Login respone parsing in case of Error...
+      else if (profileServiceData.responseData === null) {
+        props.userProfileResponseHandler({
+          isProfileDataFlag: false,
+          profileData: null,
+          errorMessage: profileServiceData.responseError.message,
+        });
+      }
+    },
+  });
+};
+const ProfileDataController = { GetUserProfileDetails };
+export default ProfileDataController;
